4c35ddb2b7c7622931e746554ef9439f7211dc91
ahmedhamidibr <129907102+ahmedhamidibr@users.noreply.github.com>
2023-07-17 14:36:00 +0300

    Update test_rectangle.py

--- test_rectangle.py
+++ test_rectangle.py
@@ -1,6 +1,6 @@
 from rectangle import rectangle_area
 
 def test_rectangle_area():
-    assert rectangle_area(3, 3) == 6
+    assert rectangle_area(3, 2) == 6
     assert rectangle_area(0, 4) == 0
     assert rectangle_area(5, 5) == 25
